a73b1f7c24e70f12f2f242c28e41c3eed8fb339b
Darren Lester <dsslester@gmail.com>
2017-02-25 18:56:09 +0000

    Bind parser functions rather than wrap

--- src/parsers.js
+++ src/parsers.js
@@ -7,7 +7,7 @@
 const analysers = require("./analysers");
 
 // returns an array of all characters in a string that fulfill a condition
-function basicParser(str, condition) {
+function basicParser(condition, str) {
     let result = [];
 
     for (let i = 0; i < str.length; ++i) {
@@ -21,7 +21,7 @@ function basicParser(str, condition) {
 
 // returns an array of all sequences of characters in a string that fulfill
 // a condition
-function accumulativeParser(str, condition) {
+function accumulativeParser(condition, str) {
     let accumulations = [];
     let accumulator = "";
 
@@ -39,41 +39,11 @@ function accumulativeParser(str, condition) {
     return accumulations;
 }
 
-// return array of all hiragana in a string
-function parseHiragana(str) {
-    return basicParser(str, analysers.isHiragana);
-}
-
-// return array of all katakana in a string
-function parseKatakana(str) {
-    return basicParser(str, analysers.isKatakana);
-}
-
-// return array of all kana in a string
-function parseKana(str) {
-    return basicParser(str, analysers.isKana);
-}
-
-// return array of all kanji in a string
-function parseKanji(str) {
-    return basicParser(str, analysers.isKanji);
-}
-
-// return array of all kanji compounds in a string
-function parseKanjiCompounds(str) {
-    return accumulativeParser(str, analysers.isKanji);
-}
-
-// return an array of all sections of Japanese in a string
-function parseJapanese(str) {
-    return accumulativeParser(str, analysers.isJapanese);
-}
-
 module.exports = {
-    parseHiragana: parseHiragana,
-    parseKatakana: parseKatakana,
-    parseKana: parseKana,
-    parseKanji: parseKanji,
-    parseKanjiCompounds: parseKanjiCompounds,
-    parseJapanese: parseJapanese
+    parseHiragana: basicParser.bind(null, analysers.isHiragana),
+    parseKatakana: basicParser.bind(null, analysers.isKatakana),
+    parseKana: basicParser.bind(null, analysers.isKana),
+    parseKanji: basicParser.bind(null, analysers.isKanji),
+    parseKanjiCompounds: accumulativeParser.bind(null, analysers.isKanji),
+    parseJapanese: accumulativeParser.bind(null, analysers.isJapanese)
 };
